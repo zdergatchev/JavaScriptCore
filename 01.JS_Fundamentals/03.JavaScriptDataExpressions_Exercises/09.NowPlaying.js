139e203c3575ec64bc200b0ec46fc74bcf8de382
limerixtt123 <limerix@mail.bg>
2016-09-28 06:53:56 +0300

    JavaScript Data Expressions Exercises

---
+++ 01.JS_Fundamentals/03.JavaScriptDataExpressions_Exercises/09.NowPlaying.js
@@ -0,0 +1,5 @@
+function nowPlaying(playingTrack) {
+    console.log(`Now Playing: ${playingTrack[1]} - ${playingTrack[0]} \[${playingTrack[2]}\]`);
+}
+
+nowPlaying(['Number One', 'Nelly', '4:09']);
