cec25804ddf99fa7967dd88ce68439e23174bb67
limerixtt123 <limerix@mail.bg>
2016-10-02 14:13:04 +0300

    JavaScript Arrays and Matrices Lab

---
+++ 01.JS_Fundamentals/07.JavaCriptArraysАndMatrices/04.FirstАndLastKNumbers.js
@@ -0,0 +1,8 @@
+function firstLastKElements(arr) {
+    let k = Number(arr.shift());
+    console.log(arr.slice(0, k).join(' '));
+    console.log(arr.slice(arr.length-k,
+        arr.length).join(' '));
+}
+
+firstLastKElements(['3', '6', '7', '8', '9']);
