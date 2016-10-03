8c6e6c83d737c5593d99297402402832265975c1
limerixtt123 <limerix@mail.bg>
2016-10-03 13:34:40 +0300

    JavaScript Strings and Regular Expressions Lab

---
+++ 01.JS_Fundamentals/08.JavaScriprStringsAndRegExp/04.ExtractText.js
@@ -0,0 +1,15 @@
+function extractTextFromParenthesis([text]){
+    let result = [];
+    let startIndex = text.indexOf('(');
+    while (startIndex > -1) {
+        let endIndex = text.indexOf(')', startIndex);
+        if (endIndex == -1)
+            break;
+        let snippet = text.substring(startIndex +1, endIndex);
+        result.push(snippet);
+        startIndex = text.indexOf('(', endIndex);
+    }
+    console.log(result.join(', '));
+}
+
+extractTextFromParenthesis(['Rakiya (Bulgarian brandy) is home-made liquor (alcoholic drink). It can be made of grapes, plums or other fruits (even apples).']);
