5221c10fd2b46c4102f40062fbe2395dc8aab834
limerixtt123 <limerix@mail.bg>
2016-10-03 13:33:51 +0300

    JavaScript Strings and Regular Expressions Lab

---
+++ 01.JS_Fundamentals/08.JavaScriprStringsAndRegExp/02.ConcatenateAndReverse.js
@@ -0,0 +1,9 @@
+function concatenateAndReverse(arr){
+   let allStrings = arr.join('');
+    let chars = Array.from(allStrings);
+    let revChars = chars.reverse();
+    let revStr = revChars.join('');
+    return revStr;
+}
+
+console.log(concatenateAndReverse(['I', 'am', 'student']));
