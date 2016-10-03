7ef318ec7518a5bbbff1f0a6739363919e2054dd
limerixtt123 <limerix@mail.bg>
2016-10-03 13:58:56 +0300

    JavaScript Strings and Regular Expressions Lab

--- 01.JS_Fundamentals/08.JavaScriprStringsAndRegExp/10.MatchAllWords.js
+++
@@ -1,7 +0,0 @@
-function matchAllWords(text) {
-    if (Array.isArray(text)) text = text[0];
-    let words = text.match(/\w+/g);
-    return words.join('|');
-}
-
-console.log(matchAllWords(['_ (Underscores) are also word characters!']));
