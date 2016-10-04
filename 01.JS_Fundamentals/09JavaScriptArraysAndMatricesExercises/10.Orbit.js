b8970007f4e6c867cf1cb18a37e4430352d1804a
limerixtt123 <limerix@mail.bg>
2016-10-04 04:49:51 +0300

    JavaScript Arrays and Matrices Exercises

---
+++ 01.JS_Fundamentals/09JavaScriptArraysAndMatricesExercises/10.Orbit.js
@@ -0,0 +1,39 @@
+function diagonalAttack(matrixRows) {
+    let matrixInput = matrixRows.map(
+        row => row.split(' ').map(Number));
+    let sizeMatrix = Array.from(matrixInput[0]);
+    let rows = Number(sizeMatrix[0]);
+    let cols = Number(sizeMatrix[1]);
+
+
+    let cordPoint = Array.from(matrixInput[1]);
+    let rowPoint = Number(cordPoint[0]);
+    let colPoint = Number(cordPoint[1]);
+
+    let matrix = [];
+    for (let row = 0; row < rows; row++) {
+        matrix[row] = [];
+        for (let col = 0; col < cols; col++) {
+            matrix[row][col] = 0;
+        }
+    }
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix.length; col++) {
+            matrix[rowPoint][colPoint] = 1;
+            if (row != rowPoint && col == colPoint) {
+                matrix[row][col] = Math.abs(row - rowPoint) + 1;
+            }
+            else if (row == rowPoint && col != colPoint) {
+                matrix[row][col] = Math.abs(col - colPoint) + 1;
+            }
+            else if(row != rowPoint || col != colPoint){
+                matrix[row][col] = Math.max(Math.abs(row - rowPoint), Math.abs(col - colPoint)) + 1;
+            }
+        }
+    }
+    return matrix.map(row => row.join(' ')).join('\n');
+}
+
+console.log(diagonalAttack(['4 4', '0 0']));
+console.log(diagonalAttack(['5 5', '2 2']));
+console.log(diagonalAttack(['3 3', '2 2']));
