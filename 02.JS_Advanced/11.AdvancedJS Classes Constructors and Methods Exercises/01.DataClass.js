572e6cd9ccce2dc6e646b9b5fb8b814814f2763b
limerixtt123 <limerix@mail.bg>
2016-11-07 08:44:17 +0200

    JS Advanced Classes Constructors and Methods Exercoses

---
+++ 02.JS_Advanced/11.AdvancedJS Classes Constructors and Methods Exercises/01.DataClass.js
@@ -0,0 +1,10 @@
+class Requester{
+    constructor(method, uri, version, message){
+        this.method = method;
+        this.uri = uri;
+        this.version = version;
+        this.message = message;
+        this.response = undefined;
+        this.fulfilled = false;
+    }
+}
