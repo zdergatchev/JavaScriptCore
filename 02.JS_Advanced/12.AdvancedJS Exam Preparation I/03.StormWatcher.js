850425cc0bf2ba6e333fbf3b3061ff0ca9f4eede
limerixtt123 <limerix@mail.bg>
2016-11-08 08:12:07 +0200

    JS Advanced Exam Preparation I

---
+++ 02.JS_Advanced/12.AdvancedJS Exam Preparation I/03.StormWatcher.js
@@ -0,0 +1,28 @@
+(function () {
+    let nextId = 0;
+
+    return class Report {
+        constructor(temperature, humidity, pressure, windSpeed) {
+            this.id = nextId++;
+            this.temperature = temperature;
+            this.humidity = humidity;
+            this.pressure = pressure;
+            this.windSpeed = windSpeed;
+        }
+
+        toString() {
+            let status = 'Not stormy';
+            if (this.temperature < 20 && (this.pressure < 700 || this.pressure > 900) && this.windSpeed > 25) {
+                status = 'Stormy';
+            }
+
+            let result = `Reading ID: ${this.id}
+Temperature: ${this.temperature}*C
+Relative Humidity: ${this.humidity}%
+Pressure: ${this.pressure}hpa
+Wind Speed: ${this.windSpeed}m/s
+Weather: ${status}`;
+            return result;
+        }
+    }
+})()
