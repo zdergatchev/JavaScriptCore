f65946b1a51c9ee5095875f829663049f1b23060
limerixtt123 <limerix@mail.bg>
2016-11-09 09:12:09 +0200

    JS Advanced Class Inheritance Exercise

---
+++ 02.JS_Advanced/14.AdvancedJS Class Inheritance Exercise/07.Mixins.js
@@ -0,0 +1,40 @@
+function createMixins() {
+
+    let computerQualityMixin = function (classToExtend) {
+        classToExtend.prototype.getQuality = function () {
+            return (this.processorSpeed + this.ram + this.hardDiskSpace)/3;
+        }
+        classToExtend.prototype.isFast = function () {
+            if(this.processorSpeed > (this.ram / 4)){
+                return true;
+            }
+            return false;
+        }
+        classToExtend.prototype.isRoomy = function () {
+            if(this.hardDiskSpace > Math.floor(this.ram * this.processorSpeed)){
+                return true;
+            }
+            return false;
+        }
+    };
+
+    let styleMixin = function (classToExtend) {
+        classToExtend.prototype.isFullSet =function () {
+            if(this.manufacturer == this.keyboard.manufacturer && this.manufacturer == this.monitor.manufacturer){
+                return true;
+            }
+            return false;
+
+        }
+        classToExtend.prototype.isClassy = function () {
+            if(this.battery.expectedLife >= 3 && (this.color == 'Silver' || this.color == 'Black') && this.weight < 3){
+                return true;
+            }
+            return false;
+        }
+    };
+    return {
+        computerQualityMixin,
+        styleMixin
+    }
+}
