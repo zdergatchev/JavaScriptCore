1225450761de5e3a6366a3d30b8cc9bb1f888250
limerixtt123 <limerix@mail.bg>
2016-11-11 10:45:28 +0200

    JS Advanced Class Modules Exercises

---
+++ 02.JS_Advanced/16.Modules Exercises/03.Turtles/app.js
@@ -0,0 +1,11 @@
+let Turtle = require('./Turtle.js');
+let WaterTurtle = require('./waterTurtle.js');
+let GalapagosTurtle = require('./galapagosTurtle.js');
+let EvkodianTurtle = require('./evkodianTurtle.js');
+let NinjaTurtle = require('./ninjaTurtle.js');
+
+result.Turtle = Turtle;
+result.WaterTurtle = WaterTurtle;
+result.GalapagosTurtle = GalapagosTurtle;
+result.EvkodianTurtle = EvkodianTurtle;
+result.NinjaTurtle = NinjaTurtle;
