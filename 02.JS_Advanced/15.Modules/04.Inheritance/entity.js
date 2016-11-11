8fc83a5749773b6f2111ad9bb85a580335cb7d05
limerixtt123 <limerix@mail.bg>
2016-11-11 08:48:57 +0200

    JS Advanced Modules Lab

---
+++ 02.JS_Advanced/15.Modules/04.Inheritance/entity.js
@@ -0,0 +1,10 @@
+class Entity {
+    constructor (name) {
+        if (new.target === Entity) {
+            throw new Error;
+        }
+        this.name = name;
+    }
+}
+
+module.exports.Entity = Entity;
