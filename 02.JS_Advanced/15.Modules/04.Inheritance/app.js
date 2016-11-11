31153a78bd86aa5b1443ab09943ab042a19b109f
limerixtt123 <limerix@mail.bg>
2016-11-11 08:48:03 +0200

    JS Advanced Modules Lab

---
+++ 02.JS_Advanced/15.Modules/04.Inheritance/app.js
@@ -0,0 +1,9 @@
+let Entity = require('./entity').Entity;
+let Person = require('./person').Person;
+let Dog = require('./dog').Dog;
+let Student = require('./student').Student;
+
+result.Entity = Entity;
+result.Person = Person;
+result.Dog = Dog;
+result.Student = Student;
