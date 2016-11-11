961eb4f91b09a7d0ad60b52e74fc5f91e49d2e58
limerixtt123 <limerix@mail.bg>
2016-11-11 10:51:50 +0200

    JS Advanced Class Modules Exercises

---
+++ 02.JS_Advanced/16.Modules Exercises/06.ModularDOM/app.js
@@ -0,0 +1,13 @@
+let BaseElement = require('./baseElement.js');
+let TitleBar = require('./titleBar.js');
+let Footer = require('./footer.js');
+let Article = require('./article.js');
+let ImageArticle = require('./imageArticle.js');
+let TableArticle = require('./tableArticle.js');
+
+result.BaseElement = BaseElement;
+result.TitleBar = TitleBar;
+result.Footer = Footer;
+result.Article = Article;
+result.ImageArticle = ImageArticle;
+result.TableArticle = TableArticle;
