3ad0776e1fcae6dc9eb3c31c706428a916aad7c8
limerixtt123 <limerix@mail.bg>
2016-11-11 10:52:48 +0200

    JS Advanced Class Modules Exercises

---
+++ 02.JS_Advanced/16.Modules Exercises/06.ModularDOM/footer.js
@@ -0,0 +1,13 @@
+let BaseElement = require('./baseElement.js');
+
+class Footer extends BaseElement{
+    constructor(message){
+        super();
+        this.message = message;
+    }
+
+    getElementString(){
+        return `<footer>Copyright &copy; ${this.message}</footer>`;
+    }
+}
+module.exports = Footer;
